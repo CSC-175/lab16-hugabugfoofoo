e4bd12364860dbbcb2338cb4304b8c322a376635
Noah <hugabug@umich.edu>
2025-03-26 13:08:58 -0400

    First Commit

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,13 @@
 // Code to implement the gcf function goes here
+#include <cmath>
+#include <iostream>
+using namespace std;
+int gcf(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+
+    }
+    return a;
+}
